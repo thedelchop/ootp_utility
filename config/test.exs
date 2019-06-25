64cbbea7f975de9e429a53d381ca24440bfec55a
Joseph DelCioppio <jdelcioppio@mojotech.com>
2019-06-25 12:59:43 -0400

    Remove incorrect credentials from Test DB config

--- config/test.exs
+++ config/test.exs
@@ -2,8 +2,6 @@ use Mix.Config
 
 # Configure your database
 config :ootp_utility, OOTPUtility.Repo,
-  username: "postgres",
-  password: "postgres",
   database: "ootp_utility_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
