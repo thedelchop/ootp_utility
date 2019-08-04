5e308113258db7b1d6b2f552901e9a18233bd5f2
Joseph DelCioppio <jdelcioppio@mojotech.com>
2019-08-04 19:03:53 -0400

    Add Teams.Record schema

---
+++ lib/ootp_utility/teams/record.ex
@@ -0,0 +1,27 @@
+defmodule OOTPUtility.Teams.Record do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "team_records" do
+    field :games, :integer
+    field :games_behind, :float
+    field :losses, :integer
+    field :magic_number, :integer
+    field :position, :integer
+    field :streak, :integer
+    field :winning_percentage, :float
+    field :wins, :integer
+    field :game_date, :date
+
+    belongs_to :team, OOTPUtility.Leagues.Team, foreign_key: :team_id, references: :team_id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(record, attrs) do
+    record
+    |> cast(attrs, [:games, :wins, :losses, :position, :winning_percentage, :games_behind, :streak, :magic_number, :team_id])
+    |> validate_required([:games, :wins, :losses, :position, :winning_percentage, :games_behind, :streak, :magic_number, :team_id])
+  end
+end
---
+++ priv/repo/migrations/20190804225950_create_team_records.exs
@@ -0,0 +1,24 @@
+defmodule OOTPUtility.Repo.Migrations.CreateTeamRecords do
+  use Ecto.Migration
+
+  def change do
+    create table(:team_records) do
+      add :games, :integer
+      add :wins, :integer
+      add :losses, :integer
+      add :position, :integer
+      add :winning_percentage, :float
+      add :games_behind, :float
+      add :streak, :integer
+      add :magic_number, :integer
+      add :game_date, :date
+
+      add :team_id, references(:teams, column: :team_id, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:team_records, [:team_id])
+    create index(:team_records, [:game_date])
+  end
+end
