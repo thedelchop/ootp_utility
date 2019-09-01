5bd4b32b4f9962f9befa2194827e60d26f611342
Joseph DelCioppio <jdelcioppio@mojotech.com>
2019-09-01 11:29:00 -0400

    GameLog.Line imports use new Imports module

--- lib/ootp_utility/imports/game/log/line.ex
+++ lib/ootp_utility/imports/game/log/line.ex
@@ -1,5 +1,5 @@
 defmodule OOTPUtility.Imports.Game.Log.Line do
-  import OOTPUtility.Imports
+  alias OOTPUtility.Imports
   alias OOTPUtility.Game.Log.Line
 
   @headers [
@@ -9,29 +9,37 @@ defmodule OOTPUtility.Imports.Game.Log.Line do
     "text"
   ]
 
-  @def """
-  Accepts a line of CSV representing a game log line.
+  @attribute_names [
+    :game_id,
+    :type,
+    :line,
+    :raw_text
+  ]
 
-  NOTE: The second pattern match is neccesary because the output from OOTP does not
-        correctly encode commas that exist in the game text, causing the decoder to
-        break the line into more entries than there are headers
-  """
-  @spec csv_to_changeset([String.t()]) :: %Line{}
-  def csv_to_changeset([game_id, type, line, text | rest_of_text]) do
-    %{
-      game_id: String.to_integer(game_id),
-      type: String.to_integer(type),
-      line: String.to_integer(line),
-      raw_text: Enum.join([text | rest_of_text], ",")
-    }
+  @spec import_from_path(Path.t()) :: {String.t(), integer}
+  def import_from_path(path) do
+    path
+    |> Imports.read_from_path()
+    |> Imports.trim_headers(@headers)
+    |> Imports.build_attributes_map(@attribute_names, &csv_to_changeset/1)
+    |> Imports.insert_into_database(Line)
   end
 
-  def csv_to_changeset([game_id, type, line, text]) do
-    %{
-      game_id: String.to_integer(game_id),
-      type: String.to_integer(type),
-      line: String.to_integer(line),
-      raw_text: text
-    }
+  defp csv_to_changeset([game_id, type, line, text]) do
+    [
+      String.to_integer(game_id),
+      String.to_integer(type),
+      String.to_integer(line),
+      text
+    ]
+  end
+
+  defp csv_to_changeset([game_id, type, line, text | rest_of_text]) do
+    [
+      String.to_integer(game_id),
+      String.to_integer(type),
+      String.to_integer(line),
+      Enum.join([text | rest_of_text], ",")
+    ]
   end
 end
