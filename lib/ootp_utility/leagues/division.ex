3e99f32b5937b74710600be82a2d6001e0ffbe20
Joseph DelCioppio <jdelcioppio@mojotech.com>
2019-09-12 15:18:11 -0400

    Update Division to use Binary ID

--- lib/ootp_utility/leagues/division.ex
+++ lib/ootp_utility/leagues/division.ex
@@ -1,22 +1,15 @@
 defmodule OOTPUtility.Leagues.Division do
-  use OOTPUtility.Schema
+  use OOTPUtility.Schema, composite_key: [:league_id, :conference_id, :division_id]
 
   alias OOTPUtility.{League, Team}
   alias OOTPUtility.Leagues.Conference
 
-  @primary_key {:division_id, :id, autogenerate: false}
   schema "divisions" do
     field :name, :string
 
-    belongs_to :league, League,
-      references: :league_id,
-      foreign_key: :league_id
-
-    belongs_to :conference, Conference,
-      references: :conference_id,
-      foreign_key: :conference_id
-
-    has_many :teams, Team, foreign_key: :team_id, references: :division_id
+    belongs_to :league, League
+    belongs_to :conference, Conference
+    has_many :teams, Team
   end
 
   @doc false
--- priv/repo/migrations/20190804221653_create_divisions.exs
+++ priv/repo/migrations/20190804221653_create_divisions.exs
@@ -2,15 +2,13 @@ defmodule OOTPUtility.Repo.Migrations.CreateDivisions do
   use Ecto.Migration
 
   def change do
-    create table(:divisions, primary_key: false) do
-      add :division_id, :integer, primary_key: true
+    create table(:divisions) do
       add :name, :string
 
-      add :league_id, references(:leagues, column: :league_id, on_delete: :nothing)
-      add :conference_id, references(:conferences, column: :conference_id, on_delete: :nothing)
+      add :league_id, references(:leagues, on_delete: :nothing)
+      add :conference_id, references(:conferences, on_delete: :nothing)
     end
 
     create index(:divisions, [:league_id])
-    create index(:divisions, [:conference_id])
   end
 end
