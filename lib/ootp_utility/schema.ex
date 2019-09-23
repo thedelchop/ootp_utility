e1c9b5fbb7838b7fa639aa747dac9485fbda0b4c
Joseph DelCioppio <jdelcioppio@mojotech.com>
2019-09-23 09:45:22 -0400

    Refactor OOTPUtility.Schema

--- lib/ootp_utility/schema.ex
+++ lib/ootp_utility/schema.ex
@@ -1,40 +1,24 @@
 # Define a module to be used as base to handle composite keys
 defmodule OOTPUtility.Schema do
-  defmacro __using__(composite_key: keys) do
+  defmacro __using__(params) do
     quote do
       use Ecto.Schema
       import Ecto.Changeset
       @primary_key {:id, :string, autogenerate: false}
       @foreign_key_type :id
 
-      @composite_keys unquote(keys)
+      unless is_nil(unquote(params)) do
+        def generate_composite_key(struct),
+          do: OOTPUtility.Schema.generate_composite_key(struct, unquote(params[:composite_key]))
 
-      def generate_composite_key(%{} = struct) do
-        @composite_keys
-        |> Enum.map(&Map.get(struct, &1))
-        |> Enum.join("-")
+        defoverridable generate_composite_key: 1
       end
-
-      defoverridable generate_composite_key: 1
     end
   end
 
-  defmacro __using__(_) do
-    quote do
-      use Ecto.Schema
-      import Ecto.Changeset
-      @primary_key {:id, :string, autogenerate: false}
-      @foreign_key_type :id
-
-      @composite_keys [:id]
-
-      def generate_composite_key(%{} = struct) do
-        @composite_keys
-        |> Enum.map(&Map.get(struct, &1))
-        |> Enum.join("-")
-      end
-
-      defoverridable generate_composite_key: 1
-    end
+  def generate_composite_key(struct, keys) do
+    keys
+    |> Enum.map(&Map.get(struct, &1))
+    |> Enum.join("-")
   end
 end
