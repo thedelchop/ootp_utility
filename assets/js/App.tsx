3bec1a8a00ef5d1cbe51f698bbcc92ce3c34e7ff
Joseph DelCioppio <jdelcioppio@mojotech.com>
2019-11-02 15:41:45 -0400

    Add CssBaseline for better Material UI styling

--- assets/js/App.tsx
+++ assets/js/App.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Root from 'components/Root';
 
 // This code starts up the React app when it runs in a browser. It sets up the routing configuration and injects the app into a DOM element.
-ReactDOM.render(<Root />, document.getElementById('react-app'));
+ReactDOM.render(
+  <Fragment>
+    <CssBaseline />
+    <Root />
+  </Fragment>,
+  document.getElementById('react-app')
+);
