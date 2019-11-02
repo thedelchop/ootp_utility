60427ff64b8a3a4e1e1650cdaa81f245f8e32cee
Joseph DelCioppio <jdelcioppio@mojotech.com>
2019-11-02 15:42:25 -0400

    Add MaterialUI elements to Root to ensure setup
    Add a simple button to the Root element to confirm that MaterialUI is
    setup successfully.

--- assets/js/components/Root.tsx
+++ assets/js/components/Root.tsx
@@ -1,5 +1,13 @@
 import React, { FunctionComponent } from 'react';
+import Button from '@material-ui/core/Button';
+import {Grid} from '@material-ui/core';
 
-const Root: FunctionComponent = () => <h1>Hello World</h1>;
+const Root: FunctionComponent = () => (
+  <Grid>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  </Grid>
+);
 
 export default Root;
