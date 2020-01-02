63a05929bd993e2ccc942bd7edde427c697b0d7d
Joseph DelCioppio <jdelcioppio@mojotech.com>
2020-01-02 11:47:40 -0500

    Render simple trade card at Root for testing

--- assets/js/components/Root.tsx
+++ assets/js/components/Root.tsx
@@ -1,13 +1,57 @@
 import React, { FunctionComponent } from 'react';
-import Button from '@material-ui/core/Button';
-import {Grid} from '@material-ui/core';
 
-const Root: FunctionComponent = () => (
-  <Grid>
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  </Grid>
-);
+import TradeProposalCard, { TradeProposal } from 'components/Trades/Proposal';
+
+const proposalData: TradeProposal = {
+  initiator: {
+    name: 'Cincinnati Reds',
+    logo: 'reds',
+    assets: [
+      {
+        id: '1',
+        name: 'Shawn Lewis',
+        position: 'SP',
+        team: 'Cincinnati Reds',
+        level: 'MLB'
+      },
+      {
+        id: '2',
+        name: 'Rod Griffin',
+        position: 'CF',
+        team: 'Poland Whiskeyjacks',
+        level: 'A-'
+      },
+      {
+        id: '3',
+        name: 'Robby Dropo',
+        position: 'SP',
+        team: 'Sarasota Reds',
+        level: 'A'
+      }
+    ]
+  },
+  recipient: {
+    name: 'New York Mets',
+    logo: 'mets',
+    assets: [
+      {
+        id: '4',
+        name: 'Myron Vazquez',
+        position: 'RF',
+        team: 'New York Mets',
+        level: 'MLB'
+      },
+      {
+        id: '5',
+        name: 'Bud Dailey',
+        position: 'SP',
+        team: 'Binghamton Mets',
+        level: 'AA'
+      }
+    ]
+  }
+};
+
+const Root: FunctionComponent = () => <TradeProposalCard {...proposalData} />;
 
 export default Root;
