f4da1f9d0ba487fc0dbdbd89d2b7855a28a2c0dd
Joseph DelCioppio <jdelcioppio@mojotech.com>
2020-01-06 08:36:57 -0500

    Cleanup types for a TradeOffer

--- assets/js/api/types.ts
+++ assets/js/api/types.ts
@@ -2,24 +2,38 @@ export type Position = 'SP' | 'C' | '1B' | '2B' | '3B' | 'SS' | 'LF' | 'CF' | 'R
 export type Level = 'MLB' | 'AAA' | 'AA' | 'A' | 'A-' | 'R';
 export type PlayerRating = 0.5 | 1 | 1.5 | 2 | 2.5 | 3 | 3.5 | 4 | 4.5 | 5;
 export type PlayerRatingType = 'Actual' | 'Potential';
+export type Cash = number;
 
-export interface TradeAsset {
+export interface Team {
+  id: string;
+  name: string;
+  logo: string;
+  city: string;
+  level: Level;
+}
+
+export interface Player {
   id: string;
   name: string;
   position: Position;
-  team: string;
-  level: Level;
+  team: Team;
   overallRating: PlayerRating;
   potentialRating: PlayerRating;
 }
 
-export interface TeamOffering {
-  name: string;
-  logo: string;
+export interface DraftPick {
+  round: number;
+  year: number;
+}
+
+export type TradeAsset = Player | DraftPick | Cash;
+
+export interface TeamOffer {
+  team: Team;
   assets: TradeAsset[];
 }
 
 export interface TradeProposal {
-  initiator: TeamOffering;
-  recipient: TeamOffering;
+  initiator: TeamOffer;
+  recipient: TeamOffer;
 }
