c553f483accff5b947670cf7f5f2ed25d34f6d25
Joseph DelCioppio <jdelcioppio@mojotech.com>
2020-01-20 20:20:06 -0500

    Move Proposal story data into its own file

--- assets/js/components/Trade/Proposal.data.tsx
+++ assets/js/components/Trade/Proposal.data.tsx
@@ -1,21 +1,4 @@
-import React from 'react';
-import { host } from 'storybook-host';
-
-import Proposal from 'components/Trade/Proposal';
-import proposalData from 'components/Trade/Proposal.data';
-
-export default {
-  component: Proposal,
-  title: 'Proposal Card',
-  // Our exports that end in "Data" are not stories.
-  excludeStories: /.*Data$/,
-  decorators: [
-    host({
-      title: 'Trade Proposal Card',
-      align: 'center'
-    })
-  ]
-};
+import { TradeProposal } from 'api/types';
 
 const proposalData: TradeProposal = {
   proposedAt: new Date(2053, 0, 12),
@@ -118,6 +101,4 @@ const proposalData: TradeProposal = {
   }
 };
 
-export const AcceptedTradeProposal = () => {
-  return <Proposal {...proposalData} />;
-};
+export default proposalData;
