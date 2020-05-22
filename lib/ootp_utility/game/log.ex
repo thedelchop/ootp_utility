dde2a366e8880d7ca8b34a877ca705446f39a268
Joseph DelCioppio <jdelcioppio@mojotech.com>
2020-05-21 21:04:01 -0400

    Add struct for Game.Log and Game.Log.new/1

--- lib/ootp_utility/game/log.ex
+++ lib/ootp_utility/game/log.ex
@@ -1,22 +1,48 @@
 defmodule OOTPUtility.Game.Log do
-  alias OOTPUtility.Game.Log.Line
-  alias OOTPUtility.Repo
-
-  import Ecto.Query, only: [where: 3, order_by: 3]
-
   @moduledoc """
   The GameLog context.
   """
+  defstruct [:game_id, :lines]
+
+  @type t() :: %__MODULE__{}
+
+  alias __MODULE__
+
+  alias OOTPUtility.{Game, Repo}
+  alias OOTPUtility.Game.Log.{Line}
 
   @doc """
-  Return all of the lines related to the specfied game, sorted by their line number.
-  This will return a log, which which describes the game in a pitch by pitch basis.
+  Returns a OOTPUtility.Game.Log.t() struct populated with the
+  list of OOTPUtility.Game.Log.Line.t() populated for the specified
+  OOTPUtility.Game.t()
+
+  ## Parameters
+
+  ### new/1
+    * game: A Game.t() struct
+
+    OR
+
+    * game_id: The :id of a Game.t()
+
+  ## Examples
+
+  iex> OOTPUtility.Game.Log.new(1)
+  %OOTPUtility.Game.Log{game_id: 1, lines: [%OOTPUtility.Game.Log.Line{game_id: 1}]}
+
+  iex> game = %OOTPUtility.Game{id: 1};
+  ...> OOTPUtility.Game.Log.new(game)
+  %OOTPUtility.Game.Log{game_id: 1, lines: [%OOTPUtility.Game.Log.Line{game_id: 1}]}
   """
-  @spec lines_for_game(integer) :: {:ok, [Line.t()]} | {:error, String.t()}
-  def lines_for_game(game_id) do
-    Line
-    |> where([l], l.game_id == ^game_id)
-    |> order_by([l], l.line)
-    |> Repo.all()
+  @spec new(Game.t() | number()) :: Log.t()
+  def new(%Game{id: game_id}) do
+    new(game_id)
+  end
+
+  def new(game_id) do
+    %Log{
+      game_id: game_id,
+      lines: game_id |> Line.for_game() |> Repo.all()
+    }
   end
 end
--- test/ootp_utility/game/log_test.exs
+++ test/ootp_utility/game/log_test.exs
@@ -1,3 +1,39 @@
 defmodule OOTPUtility.Game.LogTest do
   use OOTPUtility.DataCase
+
+  alias OOTPUtility.{Game, Fixtures}
+
+  describe "Log.new/1" do
+    setup do
+      game_lines = Enum.map(1..8, &Fixtures.create_game_log_line(%{line: &1}))
+
+      game = Game.new(1)
+
+      {:ok, lines: game_lines, game: game}
+    end
+
+    test "returns a Log.t() for the specified Game.t() id", %{
+      lines: lines_for_game,
+      game: %Game{id: game_id}
+    } do
+      %Game.Log{lines: log_lines} = Game.Log.new(game_id)
+
+      assert log_lines == lines_for_game
+    end
+
+    test "returns a Log.t() for the specified Game.t()", %{lines: lines_for_game, game: game} do
+      %Game.Log{lines: log_lines} = Game.Log.new(game)
+
+      assert log_lines == lines_for_game
+    end
+
+    test "excludes lines that are not associated with the specfied Game.t()", %{game: game} do
+      lines_for_different_game =
+        Enum.map(1..3, &Fixtures.create_game_log_line(%{game_id: 2, line: &1}))
+
+      %Game.Log{lines: log_lines} = Game.Log.new(game)
+
+      refute Enum.any?(lines_for_different_game, &Enum.member?(log_lines, &1))
+    end
+  end
 end
