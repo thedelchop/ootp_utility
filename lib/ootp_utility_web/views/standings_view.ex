d7a6c02213ecc68f47a0171e5d5b8a23ab0f93ba
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-09-17 16:58:53 -0400

    Format Standings.Division winning pct/streak

--- lib/ootp_utility_web/views/standings_view.ex
+++ lib/ootp_utility_web/views/standings_view.ex
@@ -1,3 +1,15 @@
 defmodule OOTPUtilityWeb.StandingsView do
   use OOTPUtilityWeb, :view
+
+  alias OOTPUtility.Standings.Team
+
+  def winning_percentage(%Team{winning_percentage: pct} = _standing) do
+    pct
+    |> :erlang.float_to_binary(decimals: 3)
+    |> String.trim_leading("0")
+  end
+
+  def streak(%Team{streak: streak} = _standing) when streak > 0, do: "W#{streak}"
+
+  def streak(%Team{streak: streak} = _standing), do: "L#{abs(streak)}"
 end
