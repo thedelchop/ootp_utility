fb3f18b29a34c133a7f9dc9d0c6d5a1e45ae1d0b
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-10-13 13:11:32 -0400

    Make it possible to render leagues with no conferences

--- lib/ootp_utility/standings/league.ex
+++ lib/ootp_utility/standings/league.ex
@@ -3,7 +3,7 @@ defmodule OOTPUtility.Standings.League do
   use OOTPUtility.Collectable
 
   alias OOTPUtility.{Leagues, Repo}
-  alias OOTPUtility.Standings.Conference
+  alias OOTPUtility.Standings.{Conference, Division}
 
   alias __MODULE__
 
@@ -13,12 +13,37 @@ defmodule OOTPUtility.Standings.League do
     field :league_id, :string
 
     embeds_many :conference_standings, Conference
+    embeds_many :division_standings, Division
   end
 
-  def new(%Leagues.League{conferences: nil} = league) do
+  def new(%Leagues.League{conferences: %Ecto.Association.NotLoaded{}} = league) do
     league
     |> Repo.preload(conferences: [divisions: [teams: [:record]]])
     |> new()
+  end
+
+  def new(%Leagues.League{conferences: [], divisions: %Ecto.Association.NotLoaded{}} = league) do
+    league
+    |> Repo.preload(divisions: [teams: [:record]])
+    |> new()
+  end
+
+  def new(
+        %Leagues.League{
+          name: name,
+          abbr: abbr,
+          conferences: [],
+          divisions: divisions,
+          id: league_id
+        } = _league
+      ) do
+    %League{
+      name: name,
+      abbr: abbr,
+      league_id: league_id,
+      conference_standings: [],
+      division_standings: Enum.map(divisions, &Division.new/1)
+    }
   end
 
   def new(
@@ -32,7 +57,8 @@ defmodule OOTPUtility.Standings.League do
     %League{
       name: name,
       abbr: abbr,
-      league_id: league_id, 
+      league_id: league_id,
+      division_standings: [],
       conference_standings: Enum.map(conferences, &Conference.new/1)
     }
   end
--- lib/ootp_utility_web/views/standings_view.ex
+++ lib/ootp_utility_web/views/standings_view.ex
@@ -1,7 +1,8 @@
 defmodule OOTPUtilityWeb.StandingsView do
   use OOTPUtilityWeb, :view
 
-  alias OOTPUtility.Standings.Team
+  alias OOTPUtility.Standings.{Conference, Division, League, Team}
+  alias __MODULE__
 
   def winning_percentage(%Team{winning_percentage: pct} = _standing) do
     pct
@@ -12,4 +13,24 @@ defmodule OOTPUtilityWeb.StandingsView do
   def streak(%Team{streak: streak} = _standing) when streak > 0, do: "W#{streak}"
 
   def streak(%Team{streak: streak} = _standing), do: "L#{abs(streak)}"
+
+  def child_standings(
+        %League{conference_standings: [], division_standings: standings} = _standing,
+        conn
+      ) do
+    standings
+    |> Enum.map(&render(StandingsView, "division.html", conn: conn, standings: &1))
+    |> Enum.map(&Phoenix.HTML.Safe.to_iodata/1)
+    |> raw()
+  end
+
+  def child_standings(
+        %League{conference_standings: standings} = _standing,
+        conn
+      ) do
+    standings
+    |> Enum.map(&render(StandingsView, "conference.html", conn: conn, standings: &1))
+    |> Enum.map(&Phoenix.HTML.Safe.to_iodata/1)
+    |> raw()
+  end
 end
