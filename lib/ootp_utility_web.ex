549138ff9011f3be2ce40d30bc49cdee51749a09
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-10-16 17:28:17 -0400

    Add LiveView Scaffold

--- lib/ootp_utility_web.ex
+++ lib/ootp_utility_web.ex
@@ -83,6 +83,7 @@ defmodule OOTPUtilityWeb do
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
+      import OOTPUtilityWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
