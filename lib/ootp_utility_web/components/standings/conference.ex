fd14009a14f06864d32fd07e23521cb23bc97cff
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-10-22 18:53:44 -0400

    Conferece/League componets use LiveRedirect
    Since we have now moved the League and Standings pages to use LiveView
    and not a tradtional HTTP request, we need to update the links to them
    to use LiveRedirect instead of Link.

--- lib/ootp_utility_web/components/standings/conference.ex
+++ lib/ootp_utility_web/components/standings/conference.ex
@@ -3,7 +3,7 @@ defmodule OOTPUtilityWeb.Components.Standings.Conference do
 
   alias OOTPUtility.{Standings, Leagues}
   alias OOTPUtilityWeb.Components.Standings.{Division, Teams}
-  alias Surface.Components.Link
+  alias Surface.Components.LiveRedirect
   alias OOTPUtilityWeb.Router.Helpers, as: Routes
 
   prop standings, :struct, required: true
@@ -12,11 +12,11 @@ defmodule OOTPUtilityWeb.Components.Standings.Conference do
     ~F"""
       <div class="shadow border-b border-gray-200 rounded-lg">
         <div class="col-span-2 pb-5 border-b border-gray-200">
-          <Link to={path_to_conference(@standings, @socket)}>
+          <LiveRedirect to={path_to_conference(@standings, @socket)}>
             <h3 class="mt-3 ml-3 text-md leading-6 font-medium text-gray-900">
               {name(@standings)}
             </h3>
-          </Link>
+          </LiveRedirect>
         </div>
         <div class="grid grid-cols-1 gap-4 lg:gap-8">
           {#for standings <- child_standings(@standings)}
@@ -31,13 +31,14 @@ defmodule OOTPUtilityWeb.Components.Standings.Conference do
     """
   end
 
-
-  def name(%Standings.Conference{conference: %Leagues.Conference{name: name}} = _standings), do: name
+  def name(%Standings.Conference{conference: %Leagues.Conference{name: name}} = _standings),
+    do: name
 
   def child_standings(%Standings.Conference{
-      division_standings: [],
-      team_standings: standings
-    }), do: standings
+        division_standings: [],
+        team_standings: standings
+      }),
+      do: standings
 
   def child_standings(%Standings.Conference{division_standings: standings}), do: standings
 
@@ -45,15 +46,16 @@ defmodule OOTPUtilityWeb.Components.Standings.Conference do
   def has_divisions?(_), do: true
 
   def path_to_conference(
-    %Standings.Conference{
-      conference: %Leagues.Conference{
-        league: %Leagues.League{
-          slug: league_slug
-        },
-        slug: slug
-      }} = _standings,
-         socket
-       ) do
+        %Standings.Conference{
+          conference: %Leagues.Conference{
+            league: %Leagues.League{
+              slug: league_slug
+            },
+            slug: slug
+          }
+        } = _standings,
+        socket
+      ) do
     Routes.conference_path(socket, :show, league_slug, slug)
   end
 end
--- lib/ootp_utility_web/components/standings/league.ex
+++ lib/ootp_utility_web/components/standings/league.ex
@@ -3,8 +3,8 @@ defmodule OOTPUtilityWeb.Components.Standings.League do
 
   alias OOTPUtility.{Standings, Leagues}
   alias OOTPUtilityWeb.Components.Standings.{Conference, Division}
-  alias Surface.Components.Link
   alias OOTPUtilityWeb.Router.Helpers, as: Routes
+  alias Surface.Components.LiveRedirect
 
   prop standings, :struct, required: true
 
@@ -13,11 +13,11 @@ defmodule OOTPUtilityWeb.Components.Standings.League do
       <div>
         <div class="pb-5 border-b border-gray-200">
           <div class="max-w-7xl mx-auto">
-            <Link to={path_to_league(@standings, @socket)}>
+            <LiveRedirect to={path_to_league(@standings, @socket)}>
               <h1 class="text-2xl font-semibold text-gray-900">
                 {name(@standings)}
               </h1>
-            </Link>
+            </LiveRedirect>
           </div>
         </div>
         <div class={"grid", "sm:grid-cols-1", "py-6", "px-3", "xl:grid-cols-2": has_conferences?(@standings), "xl:gap-4": has_conferences?(@standings)}>
@@ -33,7 +33,9 @@ defmodule OOTPUtilityWeb.Components.Standings.League do
     """
   end
 
-  def child_standings(%Standings.League{conference_standings: [], division_standings: standings}), do: standings
+  def child_standings(%Standings.League{conference_standings: [], division_standings: standings}),
+    do: standings
+
   def child_standings(%Standings.League{conference_standings: standings}), do: standings
 
   def name(%Standings.League{league: %Leagues.League{name: name}} = _standings), do: name
