45a096a158e389500b31ca4ea5b2becbdde2c945
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-11-10 15:32:38 -0500

    Scoreboard can now show future or past games

--- lib/ootp_utility_web/components/scoreboard.ex
+++ lib/ootp_utility_web/components/scoreboard.ex
@@ -3,15 +3,38 @@ defmodule OOTPUtilityWeb.Components.Scoreboard do
 
   alias OOTPUtilityWeb.Components.Scoreboard.Game
 
+  @sm_max_width 768
+  @md_max_width 1080
+
   prop games, :list, required: true
+  prop size, :number, default: 6
 
   def render(assigns) do
     ~F"""
-      <div class="flex bg-gray-200 p-2 justify-evenly rounded-md">
-        {#for game <- Enum.take(@games, 9)}
+      <div class="flex bg-gray-200 p-5 justify-evenly rounded-md">
+        {#for game <- Enum.take(@games, @size)}
           <Game id={game.id} game={game} />
         {/for}
       </div>
     """
   end
+
+  def handle_event("viewport_resize", %{"width" => width}, socket) do
+    socket =
+      socket
+      |> assign(
+        :size,
+        case display_size(width) do
+          :small -> 2
+          :medium -> 6
+          _ -> 8
+        end
+      )
+
+    {:no_reply, socket}
+  end
+
+  defp display_size(width) when width <= @sm_max_width, do: :small
+  defp display_size(width) when width <= @md_max_width, do: :medium
+  defp display_size(_width), do: :large
 end
--- lib/ootp_utility_web/components/scoreboard/game.ex
+++ lib/ootp_utility_web/components/scoreboard/game.ex
@@ -1,44 +1,111 @@
 defmodule OOTPUtilityWeb.Components.Scoreboard.Game do
   use Surface.LiveComponent
 
-  alias OOTPUtility.Teams.Team
-  alias OOTPUtility.Games.Game
+  alias OOTPUtility.{Games, Players, Repo, Standings, Teams}
   alias OOTPUtilityWeb.Router.Helpers, as: Routes
 
   prop game, :struct, required: true
 
   def render(assigns) do
     ~F"""
-      <div class="flex flex-col overflow-hidden flex-none bg-white border p-1 mx-1 rounded-lg">
-        {team_summary(@socket, @game.away_team)}
-        {team_summary(@socket, @game.home_team)}
+      <div class="flex flex-col overflow-hidden flex-none bg-white border p-1 mx-1 rounded-lg w-32">
+        {team_summary(@socket, @game, :away_team)}
+        {team_summary(@socket, @game, :home_team)}
         <hr />
-        <div class="text-xs text-gray-500 text-center tracking-tighter px-0.5 py-1">{start_time(@game)}</div>
+        <div class={"text-xs", "text-gray-500", "tracking-tighter", "px-0.5", "py-1", "text-center": not(is_played?(@game))}>
+          { if is_played?(@game), do: results(@game), else: start_time(@game)}
+        </div>
       </div>
     """
   end
 
-  def team_summary(socket, %Team{logo_filename: logo, abbr: abbr} = team) do
-    assigns = assign(%{__changed__: %{}}, logo: logo, abbr: abbr, record: team_record(team))
+  def results(
+        %Games.Game{
+          winning_pitcher: winning_pitcher,
+          losing_pitcher: losing_pitcher,
+          save_pitcher: save_pitcher
+        } = _game
+      ) do
+    assigns =
+      %{__changed__: %{}}
+      |> assign(
+        winning_pitcher: Players.name(winning_pitcher, :short),
+        losing_pitcher: Players.name(losing_pitcher, :short),
+        save_pitcher: Players.name(save_pitcher, :short)
+      )
 
     ~F"""
-      <div class="flex p-0.5">
-        <img class="h-5 w-5 mt-0.5" src={Routes.static_path(socket, "/images/logos/#{logo}")} alt="">
-        <h4 class="text-base tracking-tighter mx-1">{@abbr}</h4>
-        <h5 class="flex-grow flex-shrink-0 text-small text-gray-400 text-right ml-2">{@record}</h5>
+      <dl>
+        <div class="flex">
+          <dt class="text-xs">W:</dt>
+          <dd class="flex-grow text-xs text-right">{@winning_pitcher}</dd>
+        </div>
+        <div class="flex">
+          <dt class="text-xs">L:</dt>
+          <dd class="flex-grow text-right text-xs">{@losing_pitcher}</dd>
+        </div>
+        {#if not is_nil(@save_pitcher) }
+          <div class="flex">
+            <dt class="text-xs">S:</dt>
+            <dd class="flex-grow text-right text-xs">{@save_pitcher}</dd>
+          </div>
+        {/if}
+      </dl>
+    """
+  end
+
+  def is_played?(%Games.Game{played: played}), do: played
+
+  def team_summary(socket, %Games.Game{played: false} = game, team_type) do
+    %Teams.Team{logo_filename: logo, abbr: abbr} = team = Map.get(game, team_type)
+
+    %{__changed__: %{}}
+    |> assign(
+      logo: logo,
+      abbr: abbr,
+      record_or_score: team_record(team),
+      socket: socket
+    )
+    |> render_team_summary()
+  end
+
+  def team_summary(socket, %Games.Game{played: true} = game, team_type) do
+    %Teams.Team{logo_filename: logo, abbr: abbr} = Map.get(game, team_type)
+    runs_scored = Map.get(game, :"#{team_type}_runs")
+
+    %{__changed__: %{}}
+    |> assign(
+      logo: logo,
+      abbr: abbr,
+      record_or_score: runs_scored,
+      socket: socket
+    )
+    |> render_team_summary()
+  end
+
+  defp render_team_summary(assigns) do
+    ~F"""
+      <div class="flex">
+        <img class="h-5 w-5" src={Routes.static_path(@socket, "/images/logos/#{@logo}")} alt="">
+        <h4 class="text-sm tracking-tighter mx-1">{@abbr}</h4>
+        <h5 class="flex-grow flex-shrink-0 text-xs text-gray-400 text-right ml-2">{@record_or_score}</h5>
       </div>
     """
   end
 
-  defp start_time(%Game{} = _game) do
-    "Mon, 7/23 8:OO PM"
+  defp start_time(%Games.Game{date: date, time: time} = _game) do
+    gametime = DateTime.new!(date, time)
+
+    Timex.format!(gametime, "{0M}/{D} {0h12}:{0m} {AM}")
   end
 
-  def team_record(%Team{} = _team) do
-    wins = Enum.random(72..105)
+  def team_record(%Teams.Team{record: %Ecto.Association.NotLoaded{}} = team) do
+    team
+    |> Repo.preload(:record)
+    |> team_record()
+  end
 
-    losses = 162 - wins
-
+  def team_record(%Teams.Team{record: %Standings.TeamRecord{wins: wins, losses: losses}} = _team) do
     "#{wins}-#{losses}"
   end
 end
