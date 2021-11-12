27b3fcdd7826e2fbccd64bbdf30aece6b57c745f
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-11-11 23:18:46 -0500

    Refactor interface for Games.for_team/2

--- test/ootp_utility/games_test.exs
+++ test/ootp_utility/games_test.exs
@@ -22,46 +22,69 @@ defmodule OOTPUtility.GamesTest do
   end
 
   describe "for_team/2" do
-    test "returns all games related to a team when give no options" do
-      team = team_fixture(%{name: "Team"})
-      opponent = team_fixture(%{name: "Opponent"})
+    setup do
+      league = league_fixture(%{current_date: Timex.today()})
+      team = team_fixture(%{name: "Team"}, league)
 
-      games = [
-        game_fixture(%{home_team: team, away_team: opponent}),
-        game_fixture(%{home_team: opponent, away_team: team})
-      ]
-
-      assert Games.for_team(team) == games
+      {:ok, league: league, team: team}
     end
 
-    test "returns the last 10 games played by the team in the last N days when the `recent` option is given" do
-      current_date = Timex.today()
+    test "returns all games related to a team when give no options", %{team: team, league: league} do
+      games = [
+        create_game(team, league),
+        create_game(%{away_team: team}, team, league)
+      ]
 
-      league = league_fixture(%{current_date: current_date})
-      team = team_fixture(%{name: "Team", league_id: league.id})
+      assert ids_for(Games.for_team(team)) == ids_for(games)
+    end
 
-      recent_games = Enum.map([6, 4, 2], fn
-        days_ago ->
-          days_ago = Timex.Duration.from_days(days_ago)
+    test "returns the games played by the team before or on :start_date", %{team: team, league: league} do
+      old_game = create_played_game(team, league, 20)
 
-          game_fixture(%{
-            home_team: team,
-            date: Timex.subtract(current_date, days_ago),
-            played: true,
-            league: league
-          })
-        end)
+      recent_games =
+        1..10
+        |> Enum.take(3)
+        |> Enum.map(&create_played_game(team, league, &1))
 
-      old_game = game_fixture(%{
-        home_team: team,
-        date: Timex.subtract(current_date, Timex.Duration.from_days(20)),
-        league: league,
-        played: true
-      })
+      games_in_past_ten_days = Games.for_team(team, start_date: date_in_past(10))
 
-      assert Games.for_team(team, %{recent: 10}) == recent_games
+      assert Enum.sort(ids_for(games_in_past_ten_days)) == Enum.sort(ids_for(recent_games))
 
-      refute Enum.any?(Games.for_team(team, %{recent: 10}), & &1.id == old_game.id)
+      refute Enum.any?(games_in_past_ten_days, & &1.id == old_game.id)
+    end
+
+    test "returns n games specified by :limit", %{team: team, league: league} do
+      [game | _rest] = games = Enum.map(1..3, fn
+        _ -> create_game(team, league)
+      end)
+
+      assert team |> Games.for_team() |> ids_for() == ids_for(games)
+
+      assert ids_for(Games.for_team(team, limit: 1)) == [game.id]
+    end
+  end
+
+  defp ids_for(games) do
+    Enum.map(games, & &1.id)
+  end
+
+  defp create_played_game(team, league, days_ago \\ 0) do
+    create_game(%{date: date_in_past(days_ago), played: true}, team, league)
+  end
+
+  defp create_game(attrs \\ %{}, team, league)  do
+    attrs
+    |> Enum.into(%{
+      home_team: team,
+      league: league
+    })
+    |> game_fixture()
+  end
+
+  defp date_in_past(days_ago) do
+    with days_ago_duration <- Timex.Duration.from_days(days_ago) do
+      Timex.today()
+      |> Timex.subtract(days_ago_duration)
     end
   end
 end
