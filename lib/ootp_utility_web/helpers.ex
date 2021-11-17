b70f5f588743ab60c58f1c5405971ef95c203838
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-11-16 21:03:30 -0500

    Add method to provide known abbrev for statistics

--- lib/ootp_utility_web/helpers.ex
+++ lib/ootp_utility_web/helpers.ex
@@ -1,5 +1,28 @@
 defmodule OOTPUtilityWeb.Helpers do
+  @stat_abbreviations %{
+    batting_average: "avg",
+    runs: "r",
+    home_runs: "hr",
+    runs_batted_in: "rbi",
+    stolen_bases: "sb",
+    wins: "w",
+    saves: "s",
+    strikeouts: "k",
+    earned_run_average: "era",
+    whip: "whip"
+  }
+
   def friendly_date(date) do
     Timex.format!(date, "{0M}/{D}/{YYYY}")
+  end
+
+  def statistic_abbreviation(stat_name) when is_binary(stat_name) do
+    stat_name
+    |> String.to_atom()
+    |> statistic_abbreviation()
+  end
+
+  def statistic_abbreviation(stat_name) do
+    Map.fetch(@stat_abbreviations, stat_name)
   end
 end
