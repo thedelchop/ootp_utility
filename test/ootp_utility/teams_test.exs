2b0d242be88f1da108f9e40c211b913bb7b5e8c2
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-11-24 10:40:49 -0500

    Replace Team test fixtures with factories

--- test/ootp_utility/teams_test.exs
+++ test/ootp_utility/teams_test.exs
@@ -1,19 +1,19 @@
 defmodule OOTPUtility.TeamsTest do
   use OOTPUtility.DataCase
 
-  import OOTPUtility.{TeamsFixtures}
+  import OOTPUtility.Factory
 
   alias OOTPUtility.Teams
 
   describe "teams" do
-    setup do: {:ok, team: team_fixture()}
+    setup do: {:ok, team: insert(:team)}
 
     test "list_teams/0 returns all teams", %{team: team} do
-      assert Teams.list_teams() == [team]
+      assert ids_for(Teams.list_teams()) == ids_for([team])
     end
 
     test "get_team!/1 returns the team with given id", %{team: team} do
-      assert Teams.get_team!(team.id) == team
+      assert Teams.get_team!(team.id).id == team.id
     end
   end
 end
