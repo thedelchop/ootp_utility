58fe5f2614a3ec820509a27ff4e6b6d1fdb593a5
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-11-24 11:29:25 -0500

    Replace fixtures with factories in Standings test

--- test/ootp_utility/standings_test.exs
+++ test/ootp_utility/standings_test.exs
@@ -2,17 +2,15 @@ defmodule OOTPUtility.StandingsTest do
   use OOTPUtility.DataCase
 
   alias OOTPUtility.Standings
-  import OOTPUtility.{StandingsFixtures, TeamsFixtures}
+  import OOTPUtility.Factory
 
   describe "team_records" do
     setup do
-      team_record = team_record_fixture(%{}, team_fixture())
-
-      {:ok, team_record: team_record}
+      {:ok, team_record: insert(:team_record)}
     end
 
     test "get_team_record!/1 returns the team_record with given id", %{team_record: team_record} do
-      assert Standings.get_team_record!(team_record.id) == team_record
+      assert Standings.get_team_record!(team_record.id).id == team_record.id
     end
   end
 end
