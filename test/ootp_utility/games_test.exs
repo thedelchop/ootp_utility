00f964b08436ff06f582414caa27af7ab1335ade
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-11-24 10:28:07 -0500

    Replace Games.Game test fixtures with factories

--- test/ootp_utility/games_test.exs
+++ test/ootp_utility/games_test.exs
@@ -1,62 +1,68 @@
 defmodule OOTPUtility.GamesTest do
   use OOTPUtility.DataCase
 
-  import OOTPUtility.GamesFixtures
-  import OOTPUtility.LeaguesFixtures
-  import OOTPUtility.TeamsFixtures
+  import OOTPUtility.Factory
 
   alias OOTPUtility.Games
 
   describe "games" do
     setup do
-      {:ok, game: game_fixture()}
+      {:ok, game: insert(:game)}
     end
 
     test "list_games/0 returns all games", %{game: game} do
-      assert Games.list_games() == [game]
+      assert ids_for(Games.list_games()) == ids_for([game])
     end
 
     test "get_game!/1 returns the game with given id", %{game: game} do
-      assert Games.get_game!(game.id) == game
+      assert Games.get_game!(game.id).id == game.id
     end
   end
 
   describe "for_team/2" do
     setup do
-      league = league_fixture(%{current_date: Timex.today()})
-      team = team_fixture(%{name: "Team"}, league)
+      league = insert(:league, current_date: Timex.today())
+      team = insert(:team, league: league)
 
       {:ok, league: league, team: team}
     end
 
     test "returns all games related to a team when give no options", %{team: team, league: league} do
       games = [
-        create_game(team, league),
-        create_game(%{away_team: team}, team, league)
+        insert(:game, home_team: team, league: league),
+        insert(:game, away_team: team, league: league)
       ]
 
       assert ids_for(Games.for_team(team)) == ids_for(games)
     end
 
-    test "returns the games played by the team before or on :start_date", %{team: team, league: league} do
-      old_game = create_played_game(team, league, 20)
+    test "returns the games played by the team before or on :start_date", %{
+      team: team,
+      league: league
+    } do
+      past_date = date_in_past(20)
+      old_game = insert(:completed_game, league: league, date: past_date)
 
       recent_games =
         1..10
         |> Enum.take(3)
-        |> Enum.map(&create_played_game(team, league, &1))
+        |> Enum.map(
+          &insert(:completed_game, home_team: team, league: league, date: date_in_past(&1))
+        )
 
       games_in_past_ten_days = Games.for_team(team, start_date: date_in_past(10))
 
       assert Enum.sort(ids_for(games_in_past_ten_days)) == Enum.sort(ids_for(recent_games))
 
-      refute Enum.any?(games_in_past_ten_days, & &1.id == old_game.id)
+      refute Enum.any?(games_in_past_ten_days, &(&1.id == old_game.id))
     end
 
     test "returns n games specified by :limit", %{team: team, league: league} do
-      [game | _rest] = games = Enum.map(1..3, fn
-        _ -> create_game(team, league)
-      end)
+      [game | _rest] =
+        games =
+        Enum.map(1..3, fn
+          _ -> insert(:game, home_team: team, league: league)
+        end)
 
       assert team |> Games.for_team() |> ids_for() == ids_for(games)
 
@@ -64,21 +70,8 @@ defmodule OOTPUtility.GamesTest do
     end
   end
 
-  defp ids_for(games) do
-    Enum.map(games, & &1.id)
-  end
-
-  defp create_played_game(team, league, days_ago \\ 0) do
-    create_game(%{date: date_in_past(days_ago), played: true}, team, league)
-  end
-
-  defp create_game(attrs \\ %{}, team, league)  do
-    attrs
-    |> Enum.into(%{
-      home_team: team,
-      league: league
-    })
-    |> game_fixture()
+  defp ids_for(resources) do
+    Enum.map(resources, & &1.id)
   end
 
   defp date_in_past(days_ago) do
