717766a2f34e231864a26999a3f42b87016effd6
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-11-26 13:18:48 -0500

    Initial implementation of Statistics.Batting.ranking/2

--- lib/ootp_utility/statistics/batting.ex
+++ lib/ootp_utility/statistics/batting.ex
@@ -4,6 +4,36 @@ defmodule OOTPUtility.Statistics.Batting do
   """
   import Ecto.Query, warn: false
 
+  alias __MODULE__
+
   alias OOTPUtility.Repo
-  alias OOTPUtility.Statistics.Batting.{Game, Player, Team}
+  alias OOTPUtility.Teams
+
+  def ranking(%Teams.Team{id: team_id, league_id: league_id} = _team, statistic) do
+    partition = :league_id
+
+    order_by = [desc: statistic]
+
+    ranking_query =
+      from s in Batting.Team,
+        select: %{
+          position: over(rank(), :rankings),
+          count: over(count(), :partition),
+          id: s.id
+        },
+        windows: [
+          rankings: [partition_by: ^partition, order_by: ^order_by],
+          partition: [partition_by: ^partition]
+        ],
+        group_by: s.id
+
+    query =
+      from stats in Batting.Team,
+        join: r in subquery(ranking_query),
+        on: stats.id == r.id,
+        select: {r.position, r.count, field(stats, ^statistic)},
+        where: stats.team_id == ^team_id
+
+    Repo.one(query)
+  end
 end
