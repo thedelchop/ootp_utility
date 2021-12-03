b6782ca0b687f724171ec36ab6e896e0d3116a3e
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-12-03 12:15:27 -0500

    Finish first pass at Team.Rankings component

--- lib/ootp_utility_web/components/team/rankings.ex
+++ lib/ootp_utility_web/components/team/rankings.ex
@@ -2,27 +2,28 @@ defmodule OOTPUtilityWeb.Components.Team.Rankings do
   use Surface.Component
 
   alias OOTPUtility.Teams.Team
+  alias OOTPUtility.Statistics
+
+  import OOTPUtilityWeb.Helpers, only: [ordinalize: 1]
+  import Phoenix.Naming, only: [humanize: 1]
 
   prop team, :struct
 
   def render(assigns) do
     ~F"""
-      <div id="team-rankings" class="flex-initial">
-        <h3 class="text-lg text-center leading-6 font-medium text-gray-900">
-          Team Stats
-        </h3>
-        <dl class="mt-5 grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-3 md:divide-y-0 md:divide-x">
-          {#for ranking <- rankings(@team)}
+      <div>
+        <dl class="grid grid-cols-1 rounded-lg bg-gray overflow-hidden shadow divide-y divide-white-200 md:grid-cols-2 md:divide-y-0 md:divide-x">
+          {#for {stat, {rank, total, value}} <- rankings(@team)}
             <div class="px-4 py-5 sm:p-6">
               <dt class="text-base font-normal text-gray-900">
-                {ranking.name}
+                {humanize(stat)}
               </dt>
               <dd class="mt-1 flex justify-between items-baseline md:block lg:flex">
                 <div class="text-2xl font-semibold text-indigo-600">
-                  {ranking.value}
+                  {value}
                 </div>
-                <div class="px-2.5 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800 md:mt-2 lg:mt-0">
-                  {rank_in(ranking)}
+                <div class="mx-1 px-2.5 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800 md:mt-2 lg:mt-0">
+                  {ordinalize(rank)} of {total}
                 </div>
               </dd>
             </div>
@@ -33,9 +34,9 @@ defmodule OOTPUtilityWeb.Components.Team.Rankings do
   end
 
   defp rankings(%Team{} = team) do
-  end
-
-  defp rank_in(_ranking) do
-    "3rd in AL"
+    [
+      {:runs, Statistics.team_ranking(team, :runs)},
+      {:runs_allowed, Statistics.team_ranking(team, :runs_allowed)}
+    ]
   end
 end
