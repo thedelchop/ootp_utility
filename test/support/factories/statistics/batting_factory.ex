bc6d75124cffb56498c033bf19be50353d21d9f6
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-12-14 17:51:13 -0500

    BattingFactory uses new Statistics.Calcualtions module

--- test/support/factories/statistics/batting_factory.ex
+++ test/support/factories/statistics/batting_factory.ex
@@ -1,5 +1,6 @@
 defmodule OOTPUtility.Statistics.BattingFactory do
   alias OOTPUtility.Statistics.Batting
+  import OOTPUtility.Statistics, only: [calculate: 2]
 
   defmacro __using__(_opts) do
     quote do
@@ -37,11 +38,13 @@ defmodule OOTPUtility.Statistics.BattingFactory do
         doubles = Faker.random_between(20, 35) * num_of_players
         triples = Faker.random_between(1, 10) * num_of_players
         home_runs = Faker.random_between(10, 45) * num_of_players
+        hits = singles + doubles + triples + home_runs
 
         runs = Faker.random_between(60, 120) * num_of_players
 
         stolen_bases = Faker.random_between(0, 40) * num_of_players
         stolen_base_percentage = Faker.random_uniform()
+        caught_stealing = (stolen_bases / stolen_base_percentage) |> round() |> trunc()
 
         walks = Faker.random_between(20, 150) * num_of_players
         intentional_walks = Faker.random_between(0, 10) * num_of_players
@@ -52,81 +55,44 @@ defmodule OOTPUtility.Statistics.BattingFactory do
         struct!(module, %{
           id: sequence(:id, &"#{&1}"),
           level_id: 1,
-          year: fn s -> s.league.season_year end,
+          at_bats: at_bats,
+          batting_average: fn s -> calculate(s, :batting_average) end,
+          batting_average_on_balls_in_play: fn s ->
+            calculate(s, :batting_average_on_balls_in_play)
+          end,
+          catchers_interference: Faker.random_between(0, 2) * num_of_players,
+          caught_stealing: caught_stealing,
+          double_plays: Faker.random_between(5, 15) * num_of_players,
+          doubles: doubles,
+          extra_base_hits: fn s -> s.doubles + s.triples + s.home_runs end,
           games: 162,
           games_started: fn stats -> stats.games end,
-          at_bats: at_bats,
-          plate_appearances: fn s -> ceil(s.at_bats * 1.124) end,
-          hits: fn s -> hits(s) end,
-          singles: singles,
-          doubles: doubles,
-          triples: triples,
-          home_runs: home_runs,
-          extra_base_hits: fn s -> s.doubles + s.triples + s.home_runs end,
-          total_bases: fn s -> total_bases(s) end,
-          strikeouts: Faker.random_between(75, 160) * num_of_players,
-          walks: walks,
-          intentional_walks: intentional_walks,
           hit_by_pitch: hit_by_pitch,
-          catchers_interference: Faker.random_between(0, 2) * num_of_players,
+          hits: hits,
+          home_runs: home_runs,
+          intentional_walks: intentional_walks,
+          isolated_power: fn s -> calculate(s, :isolated_power) end,
+          on_base_percentage: fn s -> calculate(s, :on_base_percentage) end,
+          on_base_plus_slugging: fn s -> calculate(s, :on_base_plus_slugging) end,
+          plate_appearances: fn s -> ceil(s.at_bats * 1.124) end,
           runs: runs,
           runs_batted_in: fn s -> ceil(s.runs * 0.955) end,
-          double_plays: Faker.random_between(5, 15) * num_of_players,
+          runs_created: fn s -> calculate(s, :runs_created) end,
           sacrifice_flys: sacrifice_flys,
           sacrifices: Faker.random_between(2, 5) * num_of_players,
-          stolen_bases: stolen_bases,
+          singles: singles,
+          slugging: fn s -> calculate(s, :slugging) end,
           stolen_base_percentage: stolen_base_percentage,
-          caught_stealing: fn s ->
-            (s.stolen_bases / stolen_base_percentage) |> round() |> trunc()
-          end,
-          batting_average: fn s -> batting_average(s) end,
-          batting_average_on_balls_in_play: 0.300,
-          isolated_power: fn s -> isolated_power(s) end,
-          on_base_percentage: fn s -> obp(s) end,
-          on_base_plus_slugging: fn s -> obp(s) + slg(s) end,
-          slugging: fn s -> slg(s) end,
-          runs_created: fn s -> runs_created(s) end,
-          league: fn -> build(:league) end,
-          team: fn s -> build(:team, league: s.league) end
+          stolen_bases: stolen_bases,
+          strikeouts: Faker.random_between(75, 160) * num_of_players,
+          team: fn s -> build(:team, league: s.league) end,
+          total_bases: fn s -> calculate(s, :total_bases) end,
+          triples: triples,
+          walks: walks,
+          year: fn s -> s.league.season_year end,
+          league: fn -> build(:league) end
         })
       end
-
-      def batting_average(%{at_bats: ab} = stats), do: do_round(hits(stats) / ab)
-
-      def isolated_power(%{at_bats: ab} = stats),
-        do: do_round((total_bases(stats) - hits(stats)) / ab)
-
-      def hits(%{
-            singles: singles,
-            doubles: doubles,
-            triples: triples,
-            home_runs: home_runs
-          }) do
-        singles + doubles + triples + home_runs
-      end
-
-      def total_bases(%{
-            singles: singles,
-            doubles: doubles,
-            triples: triples,
-            home_runs: home_runs
-          }) do
-        singles + 2 * doubles + 3 * triples + 4 * home_runs
-      end
-
-      def obp(%{walks: bb, hit_by_pitch: hbp, at_bats: ab, sacrifice_flys: sf} = stats) do
-        do_round((hits(stats) + bb + hbp) / (ab + bb + hbp + sf))
-      end
-
-      def slg(%{at_bats: ab} = stats) do
-        do_round(total_bases(stats) / ab)
-      end
-
-      def runs_created(%{walks: bb, at_bats: ab} = stats) do
-        do_round((hits(stats) + bb) * total_bases(stats) / (ab + bb))
-      end
-
-      defp do_round(value), do: Float.round(value, 4)
     end
   end
 end
