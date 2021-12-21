0b6fabd89961925023a66f8b78ffcc8b8b49703d
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-12-21 17:30:43 -0500

    Update Conferece and Division names
    The names for Conference and Division are not unique, but by including
    their parent league's abbr, which are unique, we can settle on unique
    names for both conferences and divisions.

--- lib/ootp_utility/imports/leagues/conference.ex
+++ lib/ootp_utility/imports/leagues/conference.ex
@@ -10,5 +10,17 @@ defmodule OOTPUtility.Imports.Leagues.Conference do
   def should_import?(%{name: ""}), do: false
   def should_import?(_), do: true
 
-  def update_changeset(changeset), do: Leagues.Conference.put_composite_key(changeset)
+  def update_changeset(changeset) do
+    changeset
+    |> Leagues.Conference.put_composite_key()
+    |> put_name()
+  end
+
+  defp put_name(%Ecto.Changeset{changes: %{name: name, league_id: league_id}} = changeset) do
+    %Leagues.League{abbr: league_abbr} = Leagues.get_league!(league_id, preload: [])
+
+    name = if Regex.match?(~r/Conference\s[1-9]/, name), do: "#{league_abbr} #{name}", else: name
+
+    Ecto.Changeset.change(changeset, %{name: name})
+  end
 end
--- lib/ootp_utility/imports/leagues/division.ex
+++ lib/ootp_utility/imports/leagues/division.ex
@@ -14,6 +14,34 @@ defmodule OOTPUtility.Imports.Leagues.Division do
     changeset
     |> Leagues.Division.put_composite_key()
     |> put_conference_id()
+    |> put_name()
+  end
+
+  defp put_name(
+         %Ecto.Changeset{changes: %{name: name, league_id: league_id}} =
+           changeset
+       ) do
+    %Leagues.League{abbr: league_abbr} = Leagues.get_league!(league_id, preload: [])
+
+    Ecto.Changeset.change(changeset, %{name: "#{league_abbr} #{get_short_name(name)}"})
+  end
+
+  defp put_name(%Ecto.Changeset{changes: %{name: name, conference_id: conference_id}} = changeset) do
+    %Leagues.Conference{abbr: conference_abbr} = Leagues.get_conference!(conference_id)
+
+    Ecto.Changeset.change(changeset, %{name: "#{conference_abbr} #{get_short_name(name)}"})
+  end
+
+  def get_short_name(name) do
+    regex = ~r/(?<short_name>\w+)\sDivision/
+
+    if Regex.match?(regex, name) do
+      %{"short_name" => short_name} = Regex.named_captures(regex, name)
+
+      short_name
+    else
+      name
+    end
   end
 
   defp put_conference_id(%Ecto.Changeset{changes: changes} = changeset) do
