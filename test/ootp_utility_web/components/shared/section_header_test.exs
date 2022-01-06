dc7846cc898bbc285ea9b2de0e8f742b4836c354
Joseph DelCioppio <jdelcioppio@mojotech.com>
2022-01-06 16:48:33 -0500

    Add snapshot test for SectionHeader component

---
+++ test/ootp_utility_web/components/shared/section_header_test.exs
@@ -0,0 +1,13 @@
+defmodule OOTPUtilityWeb.Components.Shared.SectionHeaderTest do
+  use OOTPUtilityWeb.ComponentCase, async: true
+
+  alias OOTPUtilityWeb.Components.Shared.SectionHeader
+
+  test_snapshot "Renders a section header with the specified title" do
+    render_surface do
+      ~F"""
+        <SectionHeader>My Title</SectionHeader>
+      """
+    end
+  end
+end
