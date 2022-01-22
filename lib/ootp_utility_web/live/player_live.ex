f7d55a831bbe51425ad0e8377980a9fbb3cf4967
Joseph DelCioppio <jdelcioppio@mojotech.com>
2022-01-22 16:55:52 -0500

    Improve responsivness of PlayerLive header

--- lib/ootp_utility_web/live/player_live.ex
+++ lib/ootp_utility_web/live/player_live.ex
@@ -1,68 +1,105 @@
 defmodule OOTPUtilityWeb.PlayerLive do
   use Surface.LiveView
 
-  alias OOTPUtilityWeb.Components.Team.NameWithLogo, as: TeamNameWithLogo
+  alias Surface.Components.LiveRedirect
+
+  alias OOTPUtilityWeb.Router.Helpers, as: Routes
+  alias OOTPUtilityWeb.Components.Player.Rating
+  alias OOTPUtilityWeb.TeamLive
+
   alias OOTPUtilityWeb.Router.Helpers, as: Routes
 
-  import OOTPUtilityWeb.Helpers, only: [capitalize_all: 1]
+  import OOTPUtilityWeb.Helpers, only: [display_size: 1]
+  import OOTPUtility.Utilities, only: [position_name_to_position_key: 0]
 
   alias OOTPUtility.Players
 
   @impl true
   def render(assigns) do
     ~F"""
-      <div class="border rounded-2xl sm:p-6 lg:p-8 flex justify-between bg-white">
-        <div class="flex space-x-4">
-          <img class="h-28 w-28 rounded-full" src={Routes.static_path(@socket, "/images/default_player_photo.png")} alt="">
-          <div class="flex divide-x-1 space-x-4">
-            <div class="flex flex-col space-y-4">
-              <h1 class="text-4xl font-medium text-gray-900">{name(@player)}</h1>
-              <div class="flex flex-row items-center space-x-3 text-lg font-medium text-gray-900">
-                <TeamNameWithLogo id={"#{@player.slug}-team-header"} team={@player.team} class={"text-lg"} />
-                <h2>{"##{@player.uniform_number}"}</h2>
-                <h2>{"#{position(@player)}"}</h2>
+      <div class="border rounded-2xl p-4 md:p-8 flex flex-wrap justify-between items-center bg-white" phx-hook="WindowResize" id="player-live" >
+        <div class="flex gap-1 md:gap-2">
+          <img class="h-16 w-16 md:h-28 md:w-28 rounded-full md:mt-2" src={Routes.static_path(@socket, "/images/default_player_photo.jpg")} alt="">
+          <div class="flex flex-wrap items-center md:divide-x gap-2 md:gap-4">
+            <div class="flex flex-col gap-1 md:gap-2">
+              <h1 class="text-2xl md:text-4xl font-medium text-gray-900">{name(@player, @size)}</h1>
+              <div class="flex flex-row items-center gap-1 md:gap-2 text-base md:text-lg font-medium text-gray-900">
+                <LiveRedirect to={path_to_team(@team, @socket)} class="contents">
+                    <div class="flex-shrink-0 h-6 md:h-8 h-6 md:w-8">
+                      <img class="h-6 md:h-8 w-6 md:w-8 rounded-full" src={Routes.static_path(@socket, "/images/logos/#{@team.logo_filename}")} alt="">
+                    </div>
+                    <div class="block sm:hidden">{@team.abbr}</div>
+                    <div class="hidden sm:block">{@team.name}</div>
+                </LiveRedirect>
+                <div>{"##{@player.uniform_number}"}</div>
+                <div>{"#{position(@player)}"}</div>
               </div>
             </div>
-            <dl>
-              <div class="sm:grid sm:grid-cols-3 sm:gap-1 p-1">
+            <dl class="hidden lg:block pl-4">
+              <div class="grid grid-cols-3 gap-1 p-0.5">
                 <dt class="text-sm font-medium text-gray-500">
                   HT/WT:
                 </dt>
-                <dd class="text-sm tracking-wider text-gray-900 sm:col-span-2">
+                <dd class="text-sm tracking-wider text-gray-900 col-span-2">
                   {@player.height}/{@player.weight}
                 </dd>
               </div>
-              <div class="sm:grid sm:grid-cols-3 sm:gap-1 p-1">
+              <div class="grid grid-cols-3 gap-1 p-0.5">
                 <dt class="text-sm font-medium text-gray-500">
                   DOB:
                 </dt>
-                <dd class="text-sm tracking-wider text-gray-900 sm:col-span-2">
+                <dd class="text-sm tracking-wider text-gray-900 col-span-2">
                   {Timex.format!(@player.date_of_birth, "{0M}/{0D}/{YYYY}")} ({@player.age})
                 </dd>
               </div>
-              <div class="sm:grid sm:grid-cols-3 sm:gap-1 p-1">
+              <div class="grid grid-cols-3 gap-1 p-0.5">
                 <dt class="text-sm tracking-wider font-medium text-gray-500">
                   B/T
                 </dt>
-                <dd class="text-sm tracking-wider text-gray-900 sm:col-span-2">
+                <dd class="text-sm tracking-wider text-gray-900 col-span-2">
                   {String.capitalize(@player.bats)}/{String.capitalize(@player.throws)}
                 </dd>
               </div>
             </dl>
           </div>
         </div>
+        <Rating player={@player} class="m-auto md:m-0" />
       </div>
     """
   end
 
-  def name(%Players.Player{} = player), do: Players.name(player, :full)
+  def name(%Players.Player{} = player, size) when size in [:xsmall, :small] do
+    Players.name(player, :short)
+  end
 
-  def position(%Players.Player{position: position} = _player), do: capitalize_all(position)
+  def name(%Players.Player{} = player, _size), do: Players.name(player, :full)
+
+  def position(%Players.Player{position: position} = _player), do: Map.get(position_name_to_position_key(), position)
+
+  def path_to_team(
+        %{
+          slug: slug
+        } = _team,
+        socket
+      ) do
+    Routes.live_path(socket, TeamLive, slug)
+  end
+
+  @impl true
+  def handle_event("viewport_resize", viewport, socket) do
+    {:noreply, assign(socket, :size, display_size(viewport))}
+  end
 
   @impl true
   def mount(%{"slug" => slug}, _session, socket) do
     player = Players.get_player_by_slug(slug)
 
-    {:ok, assign(socket, :player, player)}
+    {
+      :ok,
+      socket
+      |> assign(:player, player)
+      |> assign(:team, player.team)
+      |> assign(:size, :medium)
+    }
   end
 end
