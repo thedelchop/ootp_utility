b2ddebdcf49ffd85dc191785d43fb31a453721e7
Joseph DelCioppio <jdelcioppio@mojotech.com>
2022-01-24 14:47:42 -0500

    Refactor Team.Rankings to be more mobile friendly
    Instead of worry about flipping the Ranking module to be horizontal or
    vertical depending on its size, and instead reduce the size the rest of
    the Header to be more mobile friendly.

--- lib/ootp_utility_web/components/team/rankings.ex
+++ lib/ootp_utility_web/components/team/rankings.ex
@@ -11,18 +11,18 @@ defmodule OOTPUtilityWeb.Components.Team.Rankings do
 
   def render(assigns) do
     ~F"""
-      <dl class="grid grid-cols-2 divide-y-0 divide-x m-auto md:m-0 md:grid-cols-1 md:divide-y md:divide-x-0 rounded-lg bg-gray-50 overflow-hidden shadow divide-y divide-white-200 lg:grid-cols-2 lg:divide-y-0 lg:divide-x">
+      <dl class="grid grid-cols-2 divide-x m-auto mt-2 md:m-0 rounded-lg bg-gray-50 overflow-hidden shadow divide-y divide-white-200">
         {#for {stat, {rank, total, value}} <- rankings(@team)}
           <div class="p-4">
             <dt class="text-base font-normal text-gray-900">
               {humanize(stat)}
             </dt>
-            <dd class="mt-1 flex justify-between items-baseline md:block lg:flex">
+            <dd class="mt-1 flex justify-between items-baseline">
               <div class="text-2xl font-semibold text-indigo-600">
                 {value}
               </div>
-              <div class="mx-1 px-2.5 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800 lg:mt-2">
-                {ordinalize(rank)} of {total}
+              <div class="bg-green-100 text-green-800 mx-1 px-2.5 py-0.5 rounded-full text-sm font-medium lg:mt-2">
+                {ordinalize(rank)}<span class="hidden lg:inline-flex">&nbsp;of {total}</span>
               </div>
             </dd>
           </div>
