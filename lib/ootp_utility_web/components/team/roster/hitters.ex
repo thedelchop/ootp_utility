932d660a877e4813983084d84ad0a6d758d0ea8c
Joseph DelCioppio <jdelcioppio@mojotech.com>
2022-01-28 13:08:59 -0500

    Make Team roster table more mobile friendly
    Update the Hitters and Pitchers Roster component to fix the name column
    of the roster on small screens so that the rest of the columns will
    scroll while it remains readable.

--- lib/ootp_utility_web/components/team/roster/hitters.ex
+++ lib/ootp_utility_web/components/team/roster/hitters.ex
@@ -58,95 +58,131 @@ defmodule OOTPUtilityWeb.Components.Team.Roster.Hitters do
 
   def render(assigns) do
     ~F"""
-      <div class="flex flex-col bg-white p-4 border-b border-gray-200 rounded-md shadow">
+      <div class="flex flex-col bg-white p-4 border-b border-gray-200 rounded-md shadow overflow-x-scroll">
         <SectionHeader>{@title}</SectionHeader>
-        <Table id={table_id(@title)} data={ {hitter, stats} <- @players_with_statistics } class={"px-2 py-1 lg:px-4 lg:py-2"} header_class={&header_class/2} column_class={&column_class/2}>
-          <Column label="">
-            <LiveRedirect to={path_to_player(hitter, @socket)}>
-              {Players.name(hitter, :full)}
-            </LiveRedirect>
-          </Column>
+        <div class={"relative block"}>
+          <div class={"overflow-x-scroll overflow-y-visible pb-1 ml-32"}>
+            <Table id={table_id(@title)} data={ {hitter, stats} <- @players_with_statistics } class={"px-2 py-1 lg:px-4 lg:py-2"} header_class={&header_class/2} column_class={&column_class/2}>
+              <Column label="">
+                <LiveRedirect to={path_to_player(hitter, @socket)}>
+                  {Players.name(hitter, :full)}
+                </LiveRedirect>
+              </Column>
 
-          <Column label="POS">
-            {get_position_key(hitter.position)}
-          </Column>
+              <Column label="POS">
+                {get_position_key(hitter.position)}
+              </Column>
 
-          <Column label="B">
-            {hitter.bats |> String.first() |> String.capitalize()}
-          </Column>
+              <Column label="B">
+                {hitter.bats |> String.first() |> String.capitalize()}
+              </Column>
 
-          <Column label="G">
-            {stats.games}
-          </Column>
+              <Column label="G">
+                {stats.games}
+              </Column>
 
-          <Column label="AB">
-            {stats.at_bats}
-          </Column>
+              <Column label="AB">
+                {stats.at_bats}
+              </Column>
 
-          <Column label="H">
-            {stats.hits}
-          </Column>
+              <Column label="H">
+                {stats.hits}
+              </Column>
 
-          <Column label="HR">
-            {stats.home_runs}
-          </Column>
+              <Column label="HR">
+                {stats.home_runs}
+              </Column>
 
-          <Column label="RBI">
-            {stats.runs_batted_in}
-          </Column>
+              <Column label="RBI">
+                {stats.runs_batted_in}
+              </Column>
 
-          <Column label="R">
-            {stats.runs}
-          </Column>
+              <Column label="R">
+                {stats.runs}
+              </Column>
 
-          <Column label="BB">
-            {stats.walks}
-          </Column>
+              <Column label="BB">
+                {stats.walks}
+              </Column>
 
-          <Column label="K">
-            {stats.strikeouts}
-          </Column>
+              <Column label="K">
+                {stats.strikeouts}
+              </Column>
 
-          <Column label="SB">
-            {stats.stolen_bases}
-          </Column>
+              <Column label="SB">
+                {stats.stolen_bases}
+              </Column>
 
-          <Column label="AVG">
-            {stats.batting_average |> :erlang.float_to_binary(decimals: 3) |> String.trim_leading("0")}
-          </Column>
+              <Column label="AVG">
+                {stats.batting_average |> :erlang.float_to_binary(decimals: 3) |> String.trim_leading("0")}
+              </Column>
 
-          <Column label="OBP">
-            {stats.on_base_percentage |> :erlang.float_to_binary(decimals: 3) |> String.trim_leading("0")}
-          </Column>
+              <Column label="OBP">
+                {stats.on_base_percentage |> :erlang.float_to_binary(decimals: 3) |> String.trim_leading("0")}
+              </Column>
 
-          <Column label="SLG">
-            {stats.slugging |> :erlang.float_to_binary(decimals: 3) |> String.trim_leading("0")}
-          </Column>
+              <Column label="SLG">
+                {stats.slugging |> :erlang.float_to_binary(decimals: 3) |> String.trim_leading("0")}
+              </Column>
 
-          <Column label="WAR">
-            {stats.wins_above_replacement |> :erlang.float_to_binary(decimals: 1)}
-          </Column>
-        </Table>
+              <Column label="WAR">
+                {stats.wins_above_replacement |> :erlang.float_to_binary(decimals: 1)}
+              </Column>
+            </Table>
+          </div>
+        </div>
       </div>
     """
   end
 
-  def header_class(_col, _index),
+  def header_class(_col, 0),
+    do:
+      do_header_class([
+        "text-left",
+        "font-medium",
+        "text-gray-500",
+        "bg-gray-100",
+        "absolute",
+        "top-auto",
+        "left-0",
+        "w-32",
+        "h-8"
+      ])
+
+  def header_class(_col, 1),
     do: do_header_class(["text-center", "font-medium", "text-gray-500"])
+
+  def header_class(_col, _index),
+    do: do_header_class(["text-right", "font-medium", "text-gray-500"])
 
   def do_header_class(extra_classes \\ []) do
     Enum.join(extra_classes ++ @default_header_classes, " ")
   end
 
   def column_class(_standing, 0),
-    do: do_column_class(["text-sm", "text-gray-500", "text-right"])
+    do:
+      do_column_class([
+        "text-sm",
+        "text-gray-500",
+        "text-right",
+        "absolute",
+        "top-auto",
+        "left-0",
+        "w-32",
+        "border-t",
+        "-mt-px"
+      ])
+
+  def column_class(_standing, 1),
+    do: do_column_class(["text-sm", "text-gray-500", "text-center", "border-t"])
 
   def column_class(_standing, _index),
-    do: do_column_class(["text-sm", "text-gray-500", "text-center"])
+    do: do_column_class(["text-sm", "text-gray-500", "text-right", "border-t", "tabular-nums"])
 
-  defp do_column_class(extra_classes \\ []) do
+  defp do_column_class(extra_classes) do
     Enum.join(extra_classes ++ @default_column_classes, " ")
   end
 
-  defp path_to_player(%Players.Player{slug: slug} = _player, socket), do: Routes.live_path(socket, PlayerLive, slug)
+  defp path_to_player(%Players.Player{slug: slug} = _player, socket),
+    do: Routes.live_path(socket, PlayerLive, slug)
 end
--- lib/ootp_utility_web/components/team/roster/pitchers.ex
+++ lib/ootp_utility_web/components/team/roster/pitchers.ex
@@ -64,88 +64,99 @@ defmodule OOTPUtilityWeb.Components.Team.Roster.Pitchers do
     ~F"""
       <div class="flex flex-col bg-white p-4 border-b border-gray-200 rounded-md shadow">
         <SectionHeader>{@title}</SectionHeader>
-        <Table id={table_id(@title)} data={{pitcher, stats} <- @players_with_statistics} class={"px-2 py-1 lg:px-4 lg:py-2"} header_class={&header_class/2} column_class={&column_class/2}>
-          <Column label={""}>
-            <LiveRedirect to={path_to_player(pitcher, @socket)}>
-              {Players.name(pitcher, :full)}
-            </LiveRedirect>
-          </Column>
+        <div class={"relative block"}>
+          <div class={"overflow-x-scroll overflow-y-visible pb-1 ml-32"}>
+            <Table id={table_id(@title)} data={{pitcher, stats} <- @players_with_statistics} class={"px-2 py-1 lg:px-4 lg:py-2"} header_class={&header_class/2} column_class={&column_class/2}>
+              <Column label={""}>
+                <LiveRedirect to={path_to_player(pitcher, @socket)}>
+                  <span class="md:hidden">{Players.name(pitcher, :short)}</span>
+                  <span class="hidden md:block">{Players.name(pitcher, :full)}</span>
+                </LiveRedirect>
+              </Column>
 
-         <Column label="Role">
-            {get_position_key(pitcher.position)}
-          </Column>
+            <Column label="Role">
+                {get_position_key(pitcher.position)}
+              </Column>
 
-          <Column label="T">
-            {pitcher.throws |> String.first() |> String.capitalize()}
-          </Column>
+              <Column label="T">
+                {pitcher.throws |> String.first() |> String.capitalize()}
+              </Column>
 
-          <Column label="G">
-            {stats.games}
-          </Column>
+              <Column label="G">
+                {stats.games}
+              </Column>
 
-          <Column label="GS">
-            {stats.games_started}
-          </Column>
+              <Column label="GS">
+                {stats.games_started}
+              </Column>
 
-          <Column label="IP">
-            {stats.losses}
-          </Column>
+              <Column label="IP">
+                {stats.losses}
+              </Column>
 
-          <Column label="W">
-            {stats.wins}
-          </Column>
+              <Column label="W">
+                {stats.wins}
+              </Column>
 
-          <Column label="L">
-            {stats.losses}
-          </Column>
+              <Column label="L">
+                {stats.losses}
+              </Column>
 
-          <Column label="S">
-            {stats.saves}
-          </Column>
+              <Column label="S">
+                {stats.saves}
+              </Column>
 
-          <Column label="K">
-            {stats.strikeouts}
-          </Column>
+              <Column label="K">
+                {stats.strikeouts}
+              </Column>
 
-          <Column label="BB">
-            {stats.walks}
-          </Column>
+              <Column label="BB">
+                {stats.walks}
+              </Column>
 
-          <Column label="R">
-            {stats.runs}
-          </Column>
+              <Column label="R">
+                {stats.runs}
+              </Column>
 
-          <Column label="ERA">
-            {stats.earned_run_average |> :erlang.float_to_binary(decimals: 2)}
-          </Column>
+              <Column label="ERA">
+                {stats.earned_run_average |> :erlang.float_to_binary(decimals: 2)}
+              </Column>
 
-          <Column label="WHIP">
-            {stats.walks_hits_per_inning_pitched |> :erlang.float_to_binary(decimals: 2)}
-          </Column>
+              <Column label="WHIP">
+                {stats.walks_hits_per_inning_pitched |> :erlang.float_to_binary(decimals: 2)}
+              </Column>
 
-          <Column label="WAR">
-            {stats.wins_above_replacement}
-          </Column>
-        </Table>
+              <Column label="WAR">
+                {stats.wins_above_replacement}
+              </Column>
+            </Table>
+          </div>
+        </div>
       </div>
     """
   end
 
   def header_class(_col, 0),
-    do: do_header_class(["text-left", "font-medium", "text-gray-500"])
+    do: do_header_class(["text-left", "font-medium", "text-gray-500", "bg-gray-100", "absolute", "top-auto", "left-0", "w-32", "h-8"])
+
+  def header_class(_col, 1),
+    do: do_header_class(["text-center", "font-medium", "text-gray-500"])
 
   def header_class(_col, _index),
-    do: do_header_class(["text-center", "font-medium", "text-gray-500"])
+    do: do_header_class(["text-right", "font-medium", "text-gray-500"])
 
   def do_header_class(extra_classes \\ []) do
     Enum.join(extra_classes ++ @default_header_classes, " ")
   end
 
   def column_class(_standing, 0),
-    do: do_column_class(["text-sm", "text-gray-500", "text-right"])
+    do: do_column_class(["text-sm", "text-gray-500", "text-right", "absolute", "top-auto", "left-0", "w-32", "border-t", "-mt-px"])
+
+  def column_class(_standing, 1),
+    do: do_column_class(["text-sm", "text-gray-500", "text-center", "border-t"])
 
   def column_class(_standing, _index),
-    do: do_column_class(["text-sm", "text-gray-500", "text-center"])
+    do: do_column_class(["text-sm", "text-gray-500", "text-right", "border-t", "tabular-nums"])
 
   defp do_column_class(extra_classes) do
     Enum.join(extra_classes ++ @default_column_classes, " ")
