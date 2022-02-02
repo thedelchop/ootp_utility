8e93fbcf7d35aac161ce63cdf114b3e9eefef929
Joseph DelCioppio <jdelcioppio@mojotech.com>
2022-02-01 22:26:25 -0500

    Extract Player.Header from PlayerLive view
    Extract the code to generate the header for the PlayerLive view into
    Components.Player.Header and add a snapshot test for it.

---
+++ lib/ootp_utility_web/components/player/header.ex
@@ -0,0 +1,71 @@
+defmodule OOTPUtilityWeb.Components.Player.Header do
+  use Surface.LiveComponent
+
+  alias OOTPUtility.Players
+
+  alias Surface.Components.LiveRedirect
+  alias OOTPUtilityWeb.Router.Helpers, as: Routes
+  alias OOTPUtilityWeb.Components.Player.{Details, Rating}
+  alias OOTPUtilityWeb.Components.Shared.Section
+
+  import OOTPUtilityWeb.Helpers, only: [display_size: 1]
+  import OOTPUtilityWeb.Helpers.Path, only: [path_to_team: 2]
+  import OOTPUtility.Utilities, only: [position_name_to_position_key: 0]
+
+  prop player, :struct, required: true
+  data size, :atom
+  data team, :struct
+
+  @impl true
+  def render(assigns) do
+    ~F"""
+      <Section event_target={@socket.id}>
+        <div class="flex gap-1 md:gap-2">
+          <img class="h-16 w-16 md:h-28 md:w-28 rounded-full md:mt-2" src={Routes.static_path(@socket, "/images/default_player_photo.jpg")} alt="">
+          <div class="flex flex-wrap items-center md:divide-x gap-2 md:gap-4">
+            <div class="flex flex-col gap-1 md:gap-2">
+              <h1 class="text-2xl md:text-4xl font-medium text-gray-900">{name(@player, @size)}</h1>
+              <div class="flex flex-row items-center gap-1 md:gap-2 text-base md:text-lg font-medium text-gray-900">
+                <LiveRedirect to={path_to_team(@team, @socket)} class="contents">
+                    <div class="flex-shrink-0 h-6 md:h-8 h-6 md:w-8">
+                      <img class="h-6 md:h-8 w-6 md:w-8 rounded-full" src={Routes.static_path(@socket, "/images/logos/#{@team.logo_filename}")} alt="">
+                    </div>
+                    <div class="block sm:hidden">{@team.abbr}</div>
+                    <div class="hidden sm:block">{@team.name}</div>
+                </LiveRedirect>
+                <div>{"##{@player.uniform_number}"}</div>
+                <div>{"#{position(@player)}"}</div>
+              </div>
+            </div>
+            <Details player={@player} />
+          </div>
+        </div>
+        <Rating player={@player} class="m-auto md:m-0" />
+      </Section>
+    """
+  end
+
+  def name(%Players.Player{} = player, size) when size in [:xsmall, :small] do
+    Players.name(player, :short)
+  end
+
+  def name(%Players.Player{} = player, _size), do: Players.name(player, :full)
+
+  def position(%Players.Player{position: position} = _player),
+    do: Map.get(position_name_to_position_key(), position)
+
+  @impl true
+  def handle_event("viewport_resize", viewport, socket) do
+    {:noreply, assign(socket, :size, display_size(viewport))}
+  end
+
+  def update(%{player: player} = _assigns, socket) do
+    {
+      :ok,
+      socket
+      |> assign(:player, player)
+      |> assign(:team, player.team)
+      |> assign(:size, :medium)
+    }
+  end
+end
---
+++ test/ootp_utility_web/components/player/header_test.exs
@@ -0,0 +1,37 @@
+defmodule OOTPUtilityWeb.Components.Player.HeaderTest do
+  use OOTPUtilityWeb.ComponentCase, async: true
+
+  alias OOTPUtilityWeb.Components.Player.Header
+
+  test_snapshot "renders a header for the specified player" do
+    team =
+      insert(:team,
+        name: "Boston",
+        nickname: "Red Sox",
+        logo_filename: "boston_red_sox.png"
+      )
+
+    player =
+      insert(:player,
+        first_name: "Babe",
+        last_name: "Ruth",
+        position: "LF",
+        uniform_number: 3,
+        bats: "left",
+        throws: "right",
+        height: 184,
+        weight: 205,
+        date_of_birth: ~D[1983-10-11],
+        age: 39,
+        ability: 65,
+        talent: 71,
+        team: team
+      )
+
+    render_surface do
+      ~F"""
+        <Header id={"player-header"} player={player} />
+      """
+    end
+  end
+end
