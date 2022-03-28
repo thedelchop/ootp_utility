a3ee4487fd870d40261ed91f7faf1cfda4982619
Joseph DelCioppio <jdelcioppio@mojotech.com>
2022-03-28 19:25:28 -0400

    Update app config for Fly.io release

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,6 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :ootp_utility, OOTPUtilityWeb.Endpoint,
-  url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
